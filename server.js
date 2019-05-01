04a5580e14bdd30da4c4c107d94967b429eca222
Ionică Bizău <bizauionica@gmail.com>
2019-05-01 16:50:29 +0300

    :arrow_up: 1.0.0 :tada:

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const Lien = require("lien")
+const ctx = require("ctx")
     , GitHub = require("gh.js")
     ;
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-const server = new Lien()
-    , ghClient = new GitHub(GITHUB_TOKEN)
+const server = new ctx()
+    , ghCctxt = new GitHub(GITHUB_TOKEN)
     ;
 
-server.addPage("/:owner/:name", lien => {
-    ghClient.get(`repos/${lien.params.owner}/${lien.params.name}/issues/comments`, {
+server.addPage("/:owner/:name", ctx => {
+    ghCctxt.get(`repos/${ctx.params.owner}/${ctx.params.name}/issues/comments`, {
         all: true
     }, (err, allComments) => {
-        if (err) { return lien.apiError(err); }
-        lien.end(allComments);
+        if (err) { return ctx.apiError(err); }
+        ctx.end(allComments);
     });
 });
 
