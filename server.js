29dbb1932c9429f8a28f46da2cc1e13ad6daa958
Ionică Bizău <bizauionica@gmail.com>
2023-10-01 13:07:44 +0300

    Syntax changes, and doc updates

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 const Lien = require("lien")
     , GitHub = require("gh.js")
-    ;
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 
 const server = new Lien()
     , ghClient = new GitHub(GITHUB_TOKEN)
-    ;
 
 server.addPage("/:owner/:name", ctx => {
     ghClient.get(`repos/${ctx.params.owner}/${ctx.params.name}/issues/comments`, {
         all: true
     }, (err, allComments) => {
-        if (err) { return ctx.apiError(err); }
-        ctx.end(allComments);
-    });
-});
+        if (err) { return ctx.apiError(err) }
+        ctx.end(allComments)
+    })
+})
 
 server.on("serverError", err => {
-    console.error(err.stack);
-});
+    console.error(err.stack)
+})
